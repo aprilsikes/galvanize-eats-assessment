423c73f3a14cfa23e43827f2e635bbda585e33e1
april sikes <aprilsikes@gmail.com>
2015-12-21 15:55:11 -0700

    order page

--- app/js/dom.js
+++ app/js/dom.js
@@ -2,5 +2,20 @@ $(document).ready(function() {
 
   $("button[name='placeOrder']").click(function() {
     window.location.href="pages/orders.html";
+
+    var getter = $.ajax ({
+      url: "https://galvanize-eats-api.herokuapp.com/menu",
+      method: "GET",
+      dataType: "json"
+    });
+
+    getter.done(function(response) {
+      console.log("success!");
+    });
+
+    getter.fail(function(response) {
+      console.log("fail");
+    })
   });
+
 })
