c49aa1372496240bec28308b28f9c07e0871a3c1
april sikes <aprilsikes@gmail.com>
2015-12-27 11:52:12 -0600

    fixed addition

--- app/js/dom.js
+++ app/js/dom.js
@@ -8,32 +8,24 @@ $(document).ready(function() {
 
     getter.done(function(response) {
       console.log("success");
-      var obj = {};
-      var array = [];
-      var array2 = [];
-      var newArray = [];
       var items = response["menu"];
       for (var i = 0; i < items.length; i++) {
         var foodName =  items[i].name;
         var foodPrice = items[i].price;
-        $("#selector").append("<option value='" +foodPrice+ "'>'" +foodName+ "''" +foodPrice+ "'</option>");
+        $("#selector").append("<option value='" +foodPrice+ "'>" +foodName+ "" +foodPrice+ "</option>");
       }
 
       $("#add").click(function() {
-        $("select :selected").clone().appendTo("#results");
         var arr = [];
         var arr2 = [];
-        // var total = 0;
+        $("select :selected").clone().appendTo("#order");
+
         $("select :selected").each(function() {
           arr.push($(this).val());
+        }); console.log(arr);
           for (var i = 0; i < arr.length; i++) {
-          arr2.push(Number(arr[i]));
-        }
-        }); console.log(arr2);
-        // for (var i = 0; i < arr.length; i++) {
-        //   total += parseInt(arr[i]);
-        // }
-        // $("#results").html(total);
+            arr2.push(Number(arr[i]));
+        } console.log(arr2);
         $("#total").append(getSelectedSum(arr2));
         console.log(getSelectedSum(arr2));
       });
@@ -47,9 +39,7 @@ $(document).ready(function() {
 function getSelectedSum(array) {
   var total = 0;
   for (var i = 0; i < array.length; i++) {
-    // if (foodPrice.selected) {
-      total += parseFloat(array[i]);
-    // }
+    total += parseFloat(array[i]);
   } return total.toFixed(2);
 }
 
