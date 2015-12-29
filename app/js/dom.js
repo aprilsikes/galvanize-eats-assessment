45affc6e83a7668de1dffc982d4506075507a144
april sikes <aprilsikes@gmail.com>
2015-12-29 12:29:07 -0600

    quantity

--- app/js/dom.js
+++ app/js/dom.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
       for (var i = 0; i < items.length; i++) {
         var foodName =  items[i].name;
         var foodPrice = items[i].price;
-        $("#selector").append("<option value='" +foodPrice+ "'>" +foodName+ "" +foodPrice+ "</option>");
+        $("#selector").append("<option value='" +foodPrice+ "'>" +foodName+ " " +foodPrice+ "</option>");
       }
 
       $("#add").click(function() {
@@ -21,12 +21,12 @@ $(document).ready(function() {
         $("select :selected").clone().appendTo("#order");
 
         $("select :selected").each(function() {
-          arr.push($(this).val());
+          arr.push($(this).val() * getQuantity());
         }); console.log(arr);
           for (var i = 0; i < arr.length; i++) {
             arr2.push(Number(arr[i]));
         } console.log(arr2);
-        $("#total").append(getSelectedSum(arr2));
+        $("#total").html(getSelectedSum(arr2));
         console.log(getSelectedSum(arr2));
       });
     });
@@ -36,18 +36,27 @@ $(document).ready(function() {
     });
 });
 
+var total = 0;
 function getSelectedSum(array) {
-  var total = 0;
   for (var i = 0; i < array.length; i++) {
     total += parseFloat(array[i]);
   } return total.toFixed(2);
 }
 
-function getCount(prices) {
-  var count = 0;
-  for (var i = 0; i < prices.length; i++) {
-    if (prices[i].selected) {
-      count ++;
-    }
-  } return count;
+// function getCount(prices) {
+//   var count = 0;
+//   for (var i = 0; i < prices.length; i++) {
+//     if (prices[i].selected) {
+//       count ++;
+//     }
+//   } return count;
+// }
+
+function getQuantity() {
+  var theForm = document.forms["foodForm"];
+  var quantity = theForm.elements["quan"];
+  var howMany = 0;
+  if(quantity.value != "") {
+    howMany = parseInt(quantity.value);
+  } return howMany;
 }
